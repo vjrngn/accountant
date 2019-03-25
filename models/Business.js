1baaa6f293b267fd63d8f1cf6a2c604f90a4dc28
Vijay Rangan <vijay.rangan@gmail.com>
2019-03-25 15:21:28 +0530

    model Business

---
+++ models/Business.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const schema = new Schema(
+  {
+    name: String,
+    address1: String,
+    address2: String,
+    email: String,
+    contactNumbers: [Schema.Types.Mixed],
+    city: String,
+    state: String,
+    country: String,
+    zip: String,
+    taxRegistrations: [Schema.Types.Mixed],
+    logoUrl: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model("Business", schema);
--- models/index.js
+++ models/index.js
@@ -1,7 +1,9 @@
 const User = require("./User");
 const Invoice = require("./Invoice");
+const Business = require("./Business");
 
 module.exports = {
   User,
-  Invoice
+  Invoice,
+  Business
 };
